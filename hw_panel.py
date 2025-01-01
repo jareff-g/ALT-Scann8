823ad6d94f7ed1ec2350a425146a9aa5b710f014
jareff-g <juan.remirezdeesparza@gmail.com>
2025-01-01 10:59:11 +0100

    Intermediate commit with changes in dedicated branch for Mariano's hardware panel

--- hw_panel.py
+++ hw_panel.py
@@ -9,8 +9,8 @@ __copyright__ = "Copyright 2022/24, Juan Remirez de Esparza"
 __credits__ = ["Juan Remirez de Esparza"]
 __license__ = "MIT"
 __module__ = "HwPanel"
-__version__ = "1.0.0"
-__date__ = "2024-02-24"
+__version__ = "1.0.1"
+__date__ = "2024-12-29"
 __version_highlight__ = "HwPanel - First version"
 __maintainer__ = "Juan Remirez de Esparza"
 __email__ = "jremirez@hotmail.com"
@@ -61,9 +61,23 @@ class HwPanel():
     ExitingApp = False
     active = ''
     hwpanel_after = None
-    hwpanel_first_add = 0x20
-    hwpanel_num_add = 4
+    hwpanel_first_i2c_add = 0x20
+    hwpanel_num_i2c_add = 4
     hwpanel_current_add = 0 # Round robin counter to poll all MCP23017 chip addresses
+
+    # ALT-Scann8 'buttons'
+    ALT_SCAN_8_START  = 1
+    ALT_SCAN_8_STOP = 2
+    ALT_SCAN_8_FORWARD = 3
+    ALT_SCAN_8_BACKWARD = 4
+    ALT_SCAN_8_FF = 5
+    ALT_SCAN_8_RW = 6
+
+
+    rpi_after = None
+    rpi_i2c_add = 17
+    AltScan8Callback = None
+
     CMD_GET_CNT_STATUS = 2
 
     def __new__(cls, *args, **kwargs):
@@ -71,21 +85,23 @@ class HwPanel():
             cls._instance = super().__new__(cls)
         return cls._instance
 
-    def __init__(self, master_win, i2c):
+    def __init__(self, master_win, i2c, callback):
         if not hasattr(self, 'initialized'):
             self.main_win = master_win
             self.i2c = i2c
+            self.AltScan8Callback = callback
             print(f"master_win: {master_win}, self.main_win: {self.main_win}")
             self.hwpanel_after = self.main_win.after(10, self.hwpanel_listen_loop)
+            self.rpi_after = self.main_win.after(10, self.rpi_listen_loop)
 
     def hwpanel_listen_loop(self):  # Waits for events from MCP23017 chips
         try:
-            HwPanelData = self.i2c.read_i2c_block_data(self.hwpanel_first_add+self.hwpanel_current_add, self.CMD_GET_CNT_STATUS, 5)
+            HwPanelData = self.i2c.read_i2c_block_data(self.hwpanel_first_i2c_add+self.hwpanel_current_add, self.CMD_GET_CNT_STATUS, 5)
             HwPanelTrigger = HwPanelData[0]
             HwPanelParam1 = HwPanelData[1] * 256 + HwPanelData[2]
             HwPanelParam2 = HwPanelData[3] * 256 + HwPanelData[4]  # Sometimes this part arrives as 255, 255, no idea why
             hwpanel_current_add += 1
-            hwpanel_current_add %= hwpanel_num_add
+            hwpanel_current_add %= hwpanel_num_i2c_add
         except IOError as e:
             HwPanelTrigger = 0
             # Log error to console
@@ -106,6 +122,39 @@ class HwPanel():
 
         if not self.ExitingApp:
             self.hwpanel_after = self.main_win.after(10, self.hwpanel_listen_loop)
+
+    def rpi_listen_loop(self):  # Waits for events from Raspberry Pi
+        try:
+            RPiData = self.i2c.read_i2c_block_data(self.rpi_i2c_add, self.CMD_GET_CNT_STATUS, 5)
+            RPiTrigger = RPiData[0]
+            RPiParam1 = RPiData[1] * 256 + RPiData[2]
+            RPiParam2 = RPiData[3] * 256 + RPiData[4]  # Sometimes this part arrives as 255, 255, no idea why
+        except IOError as e:
+            RPiTrigger = 0
+            # Log error to console
+            # When error is 121, not really an error, means RPi has nothing to data available for us
+            if e.errno != 121:
+                logging.warning(
+                    f"Non-critical IOError ({e}) while checking incoming event from RPi. Will check again.")
+
+        if RPiTrigger == 0:  # Do nothing
+            pass
+        # Code below just an sample, to be expanded and adjusted
+        elif RPiTrigger == ALT_SCAN_8_START:  # Button 1
+            pass
+        elif RPiTrigger == ALT_SCAN_8_STOP:
+            pass
+        elif RPiTrigger == ALT_SCAN_8_FORWARD:
+            pass
+        elif RPiTrigger == ALT_SCAN_8_BACKWARD:
+            pass
+        elif RPiTrigger == ALT_SCAN_8_FF:
+            pass
+        elif RPiTrigger == ALT_SCAN_8_RW:
+            pass
+
+        if not self.ExitingApp:
+            self.RPi_after = self.main_win.after(10, self.RPi_listen_loop)
 
     def ALT_Scann8_init_completed(self):
         pass
