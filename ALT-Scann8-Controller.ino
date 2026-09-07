2b138fbbfa8701ea9d64015cc410e9433d763a76
jareff-g <juan.remirezdeesparza@gmail.com>
2026-09-07 13:23:27 +0200

    Fixed bounds check for convergence in GetPTLevel.

--- ALT-Scann8-Controller.ino
+++ ALT-Scann8-Controller.ino
@@ -18,9 +18,9 @@ More info in README.md file
 #define __copyright__   "Copyright 2022-25, Juan Remirez de Esparza"
 #define __credits__     "Juan Remirez de Esparza"
 #define __license__     "MIT"
-#define __version__     "1.1.12"
-#define  __date__       "2026-09-06"
-#define  __version_highlight__  "Fixed convergence of max/min values for automatic PT level calculation, to avoid they converge too quickly."
+#define __version__     "1.1.14"
+#define  __date__       "2026-09-07"
+#define  __version_highlight__  "Fixed bounds check for convergence in GetPTLevel."
 #define __maintainer__  "Juan Remirez de Esparza"
 #define __email__       "jremirez@hotmail.com"
 #define __status__      "Development"
@@ -873,9 +873,9 @@ int GetLevelPT() {
     MinPT = min(PT_SignalLevelRead, MinPT);
     MaxPT_Dynamic = max(PT_SignalLevelRead*10, MaxPT_Dynamic);
     MinPT_Dynamic = min(PT_SignalLevelRead*10, MinPT_Dynamic);
-    if (MaxPT_Dynamic > (MinPT_Dynamic+5)) MaxPT_Dynamic-=1;
+    if (MaxPT_Dynamic > (MinPT_Dynamic+1)) MaxPT_Dynamic-=1;
     //if (MinPT_Dynamic < MaxPT_Dynamic) MinPT_Dynamic+=int((MaxPT_Dynamic-MinPT_Dynamic)/10);  // need to catch up quickly for overexposed frames (proportional to MaxPT to adapt to any scanner)
-    if (MinPT_Dynamic < (MaxPT_Dynamic-15)) MinPT_Dynamic+=3;  // need to catch up quickly for overexposed frames (proportional to MaxPT to adapt to any scanner)
+    if (MinPT_Dynamic < (MaxPT_Dynamic-3)) MinPT_Dynamic+=3;  // need to catch up quickly for overexposed frames (proportional to MaxPT to adapt to any scanner)
     if (PT_Level_Auto && FrameStepsDone >= int((MinFrameSteps+FrameDeductSteps)*0.9)) {
         ratio = (float)PerforationThresholdAutoLevelRatio/100;
         fixed_margin = int((MaxPT_Dynamic-MinPT_Dynamic) * 0.10);
